6fbe17da4e47b50021bae6cc02284f548f951c8e
Tolga Uzun <tolgauzun@gmail.com>
2025-09-01 20:42:53 -0400

    Update batch and send logic

--- app/jobs/send_batched_notifications_job.rb
+++ app/jobs/send_batched_notifications_job.rb
@@ -1,18 +1,61 @@
-
 class SendBatchedNotificationsJob < ActiveJob::Base
   queue_as :default
 
   def perform(issue_id)
-    pending_notifications = PendingNotification.where(issue_id: issue_id)
-    return if pending_notifications.empty?
+    lock_key = "batched_notifications_job_#{issue_id}"
+    return if Rails.cache.read(lock_key) # Skip if already running
+    Rails.cache.write(lock_key, true, expires_in: 10.minutes) # Lock for 10 min
 
-    issue = Issue.find(issue_id)
-    journal_ids = pending_notifications.pluck(:journal_id)
+    begin
+      pending_notifications = PendingNotification.where(issue_id: issue_id)
+      return if pending_notifications.empty?
 
-    # Use a custom mailer to send the batched email
-    Mailer.deliver_batched_issue_edit(issue, journal_ids)
+      issue = Issue.find(issue_id)
+      journals = Journal.where(id: pending_notifications.pluck(:journal_id))
 
-    # Delete the pending notifications
+      # Group journals by author (journal.user)
+      author_journals = Hash.new { |h, k| h[k] = [] }
+      journals.each do |journal|
+        author_journals[journal.user] << journal.id
+      end
+
+      # For each author, send batched emails to their notified users
+      author_journals.each do |author, journal_ids|
+        all_notified_users = Set.new
+        journals.where(id: journal_ids).each do |journal|
+          all_notified_users.merge(journal.notified_users)
+        end
+
+        all_notified_users.each do |user|
+          Mailer.deliver_batched_issue_edit(user, issue, journal_ids, author)
+        end
+      end
+
     pending_notifications.destroy_all
+    ensure
+      Rails.cache.delete(lock_key) # Release lock
+    end
+  end
+
+  private
+
+  def should_notify_user?(user, issue, journals)
+    return false if user.mail.blank? || user.mail_notification == 'none'
+
+    case user.mail_notification
+    when 'all'
+      true
+    when 'selected'
+      issue.watchers.include?(user) || issue.assigned_to == user || issue.author == user || journals.any? { |j| j.user == user }
+    when 'only_my_events'
+      journals.any? { |j| j.user == user }
+    when 'only_assigned'
+      issue.assigned_to == user
+    when 'only_owner'
+      issue.author == user
+    else
+      false
+    end
   end
 end
+
--- lib/mailer_patch.rb
+++ lib/mailer_patch.rb
@@ -4,28 +4,25 @@ module MailerPatch
   def self.included(base)
     base.send(:include, InstanceMethods)
     base.class_eval do
-      def self.deliver_batched_issue_edit(issue, journal_ids)
-        journals = Journal.where(id: journal_ids)
-        users = journals.map(&:notified_users).reduce(:&)
-        users.each do |user|
-          batched_issue_edit(user, issue, journal_ids).deliver_later
-        end
+      def self.deliver_batched_issue_edit(user, issue, journal_ids, author = nil)
+        batched_issue_edit(user, issue, journal_ids, author).deliver_later
       end
 
-      def batched_issue_edit(user, issue, journal_ids)
+      def batched_issue_edit(user, issue, journal_ids, author = nil)
         redmine_headers 'Project' => issue.project.identifier,
                         'Issue-Tracker' => issue.tracker.name,
                         'Issue-Id' => issue.id,
                         'Issue-Author' => issue.author.login,
-                        'Issue-Assignee' => assignee_for_header(issue)
-        redmine_headers 'Issue-Priority' => issue.priority.name if issue.priority
+                        'Issue-Assignee' => issue.assigned_to.try(:name) || ''
+        redmine_headers 'Issue-Priority' => issue.priority.try(:name) || '' if issue.priority
 
         s = "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}] "
-        s += issue.subject
+        s += "#{issue.subject}"
         @issue = issue
         @user = user
         @journals = Journal.where(id: journal_ids)
-        @issue_url = url_for(:controller => 'issues', :action => 'show', :id => issue)
+        @author = author  # Make author available in views
+        @issue_url = url_for(:controller => 'issues', :action => 'show', :id => issue.id)
 
         mail :to => user,
           :subject => s
