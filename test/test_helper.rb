dbfe8d01782295346eb16d76e9e7c7df6981a96d
Tolga Uzun <tolgauzun@gmail.com>
2025-09-23 12:20:29 -0400

    Add test dependencies

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,13 @@
 # Load the Redmine helper
 # This loads the test framework and our Redmine test environment
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
+require 'shoulda/context'
 
 # Set the fixture path for this plugin
 ActiveSupport::TestCase.fixture_paths << File.dirname(__FILE__) + "/fixtures/"
 
 # Add the plugin's lib directory to the load path
 $:.unshift File.expand_path(File.dirname(__FILE__) + '/../lib')
+
+# Use the test adapter for Active Job
+ActiveJob::Base.queue_adapter = :test
