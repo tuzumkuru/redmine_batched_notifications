36e7de15f6f7e9147618136672e62566bd2afd4a
Tolga Uzun <tolgauzun@gmail.com>
2025-09-29 13:53:42 -0400

    fix: Ensure patches are loaded reliably
    The previous patch loading mechanism using `to_prepare` with environment checks was not reliable and caused the patches to not be applied in some environments.
    
    This change simplifies the loading logic to use `Rails.application.config.after_initialize`, which is a more robust and standard way to ensure code is run after the application is initialized.

--- init.rb
+++ init.rb
@@ -6,35 +6,18 @@ Redmine::Plugin.register :redmine_batched_notifications do
   url 'https://github.com/tuzumkuru/redmine_batched_notifications'
   author_url 'https://github.com/tuzumkuru'
 
-  # Default plugin settings
   settings default: {
     'enabled' => 'true',
-    'delay' => '60' # Default delay in seconds
+    'delay' => '60'
   }, partial: 'settings/batched_notifications_settings'
 end
 
-# -----------------------------------------------------------------------------
-# Load patches
-# -----------------------------------------------------------------------------
-def load_batched_notifications_patches
-  require_dependency 'journal'
-  require_dependency 'mailer'
+Rails.application.config.after_initialize do
   require_relative 'lib/journal_patch'
   require_relative 'lib/mailer_patch'
 
-  Journal.include(JournalPatch) unless Journal.included_modules.include?(JournalPatch)
-  Mailer.include(MailerPatch) unless Mailer.included_modules.include?(MailerPatch)
+  Journal.send(:include, JournalPatch)
+  Mailer.send(:include, MailerPatch)
 
-  # Load jobs
   Dir[File.expand_path("#{__dir__}/app/jobs/**/*.rb")].each { |f| require_dependency f }
-end
-
-if Rails.env.test?
-  # In test, load immediately (to_prepare may not trigger)
-  load_batched_notifications_patches
-else
-  # In development/production, reload patches on each request (for dev reloading)
-  Rails.application.config.to_prepare do
-    load_batched_notifications_patches
-  end
 end
